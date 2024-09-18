c4f0861db464ba6e3d3d71e15c7a899f44ee9939
Richard Abishai <rickoshade1891@gmail.com>
2024-09-18 21:08:30 +0530

    Add new pages and components
    - Added DashboardPage, DevicesPage, OverviewPage, SessionsPage, and MonitoringPage to the app
    - Created components: AspectRatio, SignInPage, SignUpPage, Layout, AuthorizationPage, Skeleton, Collapsible, Label, Textarea, Separator, Progress, Toaster, Input, Checkbox
    - Updated app layout to include authorization page for signed out users

---
+++ app/(root)/monitoring/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const MonitoringPage = () => {
+  return (
+    <div>MonitoringPage</div>
+  )
+}
+
+export default MonitoringPage
---
+++ components/initial/AuthorizationPage.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AuthorizationPage = () => {
+  return (
+    <div>AuthorizationPage</div>
+  )
+}
+
+export default AuthorizationPage
