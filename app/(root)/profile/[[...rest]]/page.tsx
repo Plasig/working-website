0f1202e7a0e2c2170bfcd0c4781afcc1ea802382
Richard Abishai <rickoshade1891@gmail.com>
2024-10-01 18:45:08 +0530

    Better

--- app/(root)/profile/[[...rest]]/page.tsx
+++ app/(root)/profile/[[...rest]]/page.tsx
@@ -1,26 +1,27 @@
 import React from 'react'
 import { SignIn, UserProfile } from '@clerk/nextjs';
 import { SignedIn, SignedOut } from '@clerk/nextjs';
+
 const ProfilePage = () => {
   return (
     <main>
       <SignedIn>
-      <div className='container mx-auto px-1 py-1 max-w-2xl'>
-        <div className='bg-white shadow-md rounded-lg overflow-hidden'>
-          <UserProfile 
-            path="/profile" 
-            routing="path"
-            appearance={{
-              elements: {
-                rootBox: "max-w-full",
-                card: "shadow-none",
-                navbar: "hidden",
-                pageScrollBox: "p-2",
-              },
-            }}
-          />
+        <div className='container mx-auto px-1 py-1 max-w-2xl'>
+          <div className='bg-white shadow-md rounded-lg overflow-hidden'>
+            <UserProfile 
+              path="/profile" 
+              routing="path"
+              appearance={{
+                elements: {
+                  rootBox: "max-w-full",
+                  card: "shadow-none",
+                  navbar: "hidden",
+                  pageScrollBox: "p-2",
+                },
+              }}
+            />
+          </div>
         </div>
-      </div>
       </SignedIn>
 
       <SignedOut>
